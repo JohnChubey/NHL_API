6c4c82656fc8c631b8f462853ff362918beba929
John Chubey <johnchubey@gmail.com>
2021-03-27 20:49:45 -0300

    Add comments to utils functions

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
 def stats_exist(player_stats_list):
+    """
+    Check if the player has valid stats.
+
+    Args:
+        player_stats_list: A list containing all stats objects for a particular player returned from the API.
+    Returns:
+        A boolean value based on whether the player has valid stats or not.        
+    Raises:
+        No errors raised.
+    """
     if len(player_stats_list) > 0:
         splits = player_stats_list[0].get('splits', [])
         if len(splits) > 0:
@@ -7,4 +17,15 @@ def stats_exist(player_stats_list):
 
 
 def filter_player_data(custom_player_data):
+    """
+    Filters final player data to remove specific entries.
+
+    This function should be used to remove undesired values in the data, such as a None value.
+    Args:
+        custom_player_data: A list of data for each player, including their stats and personal information.
+    Returns:
+        A filtered list based off the given function.
+    Raises:
+        No errors raised.
+    """
     return list(filter(lambda player_stat : player_stat is not None, custom_player_data))
